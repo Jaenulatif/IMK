6f02cf53539ca308da084face1d545f4a7a6fd3c
Jaenulatif <Jaenulatif Pudin+Jaenulatif@users.noreply.github.com>
2019-04-22 13:32:11 +0700

    baru

---
+++ Iqra/Assets/Scenes/intro/move.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public void pindah()
+    {
+        Application.LoadLevel("mainMenu");
+    }
+}
---
+++ Iqra/Assets/script/bgSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgSound : MonoBehaviour
+{
+    public AudioSource Backsound;
+    
+    public void backsoundOnOff() {
+        AudioSource bgsound = Backsound.GetComponent<AudioSource>();
+    
+        if(bgsound.mute == true)
+        {
+            bgsound.mute = false;
+        }else{
+            bgsound.mute = true;
+        }
+    }
+}
